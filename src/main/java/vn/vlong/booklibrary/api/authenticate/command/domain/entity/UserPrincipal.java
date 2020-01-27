375d9abb18f12b201d8d42417349a474301bb232
Vo Hoang Long <vohoanglong07@gmail.com>
2020-01-27 18:10:52 +0700

    add authenticate

---
+++ src/main/java/vn/vlong/booklibrary/api/authenticate/command/domain/entity/UserPrincipal.java
@@ -0,0 +1,57 @@
+package vn.vlong.booklibrary.api.authenticate.command.domain.entity;
+
+import java.util.Collection;
+import java.util.Collections;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import vn.vlong.booklibrary.api.user.command.domain.entity.User;
+
+@Getter
+public class UserPrincipal implements UserDetails {
+
+  private User user;
+  private Collection<? extends GrantedAuthority> authorities;
+
+  public UserPrincipal(User user) {
+    this.user = user;
+    authorities = Collections
+        .singleton(new SimpleGrantedAuthority(user.getUserRole().getRole().name()));
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword().getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getEmail().getEmail();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return user.isActive();
+  }
+}
--- src/main/java/vn/vlong/booklibrary/api/configuration/WebSecurityConfig.java
+++ src/main/java/vn/vlong/booklibrary/api/configuration/WebSecurityConfig.java
@@ -1,16 +1,62 @@
 package vn.vlong.booklibrary.api.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import vn.vlong.booklibrary.api.authenticate.command.service.ApiUserDetailService;
+import vn.vlong.booklibrary.api.authenticate.entry.JwtAuthenticationEntryPoint;
+import vn.vlong.booklibrary.api.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  private ApiUserDetailService apiUserDetailService;
+
+  @Autowired
+  private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+  @Autowired
+  private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(apiUserDetailService).passwordEncoder(passwordEncoder());
+  }
+
+  @Bean(BeanIds.AUTHENTICATION_MANAGER)
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+    http
+        .cors().and()
+        .csrf().disable()
+        .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .authorizeRequests().antMatchers("/users/create/", "/users/active").permitAll().and()
+        .authorizeRequests().antMatchers("/auth/signin").permitAll().and()
+        .authorizeRequests().anyRequest().authenticated();
+
+    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
   }
 }
