9adeeb1f05ef831ca7a60224b7a1ecafffc81a06
Vo Hoang Long <vohoanglong07@gmail.com>
2021-02-26 14:14:33 +0700

    add eureka service

---
+++ eureka-server/src/test/java/com/vlong/booklibrary/api/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlong.booklibrary.api.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
